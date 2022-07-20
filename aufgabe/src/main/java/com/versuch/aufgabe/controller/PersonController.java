c0932f70087f4a95a4e86b09ed85cf323e711367
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-07-20 14:17:20 +0200

    Update and rename BankController.java to PersonController.java

--- aufgabe/src/main/java/com/versuch/aufgabe/controller/PersonController.java
+++ aufgabe/src/main/java/com/versuch/aufgabe/controller/PersonController.java
@@ -7,45 +7,62 @@ import java.util.List;
 import com.univocity.parsers.common.record.Record;
 import com.univocity.parsers.csv.CsvParser;
 import com.univocity.parsers.csv.CsvParserSettings;
-import com.versuch.aufgabe.repository.AccountRepository;
-import com.versuch.aufgabe.entity.AccountDetails;
-import com.versuch.aufgabe.service.BankService;
+import com.versuch.aufgabe.entity.Person;
+import com.versuch.aufgabe.service.PersonService;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.beans.factory.annotation.Autowired;
 
-
 @RestController
-@RequestMapping("/accounts")
-public class BankController {
+@RequestMapping("/")
+public class PersonController {
 
     @Autowired
-    BankService service;
+    PersonService service;
 
     @PostMapping("/upload")
     public String uploadData(@RequestParam("file") MultipartFile file) throws Exception{
-        List<AccountDetails> accountList = new ArrayList<AccountDetails>();
+        List<Person> personList = new ArrayList<Person>();
         InputStream inputStream = file.getInputStream();
         CsvParserSettings settings = new CsvParserSettings();
         settings.setHeaderExtractionEnabled(true);
         CsvParser parser = new CsvParser(settings);
         List<Record> parseAllRecords = parser.parseAllRecords(inputStream);
         parseAllRecords.forEach(record -> {
-            AccountDetails account = new AccountDetails();
-//            account.setId(Integer.parseInt(record.getString("id")));
+            Person account = new Person();
             account.setFirstName(record.getString(0));
             account.setLastName(record.getString(1));
-            account.setAddress(record.getString(2));
+            account.setZipcode(record.getString(2).substring(0,5));
+            account.setCity(record.getString(2).substring(6));
             account.setColor(record.getString(3));
-            accountList.add(account);
+            personList.add(account);
         });
-        service.saveAll(accountList);
+        service.saveAll(personList);
         return "Upload Successfull !!!";
     }
 
-    @GetMapping("/all")
-    public List<AccountDetails> allAccounts(){
+    @GetMapping("/persons")
+    public List<Person> allPersons(){
         return  service.findAll();
     }
-}
 
+    @GetMapping("/persons/{id}")
+    public Person findById(@PathVariable int id){
+        return service.findById(id);
+    }
+
+    @GetMapping("/persons/color/{color}")
+    public List<Person> findByColorId(@PathVariable int color){
+        return service.findByColorId(color);
+    }
+
+    @GetMapping("/persons/color/name/{color}")
+    public List<Person> findByColorName(@PathVariable String color){
+        return service.findByColorName(color);
+    }
+
+    @PostMapping("persons/add")
+    public Person addPerson(@RequestBody Person person){
+        return service.save(person);
+    }
+}
